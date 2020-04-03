e9d3ab3b301cc0de02d5c904b73db819bbeef009
vinhnt_ <thanhvinh2ker@gmail.com>
2020-04-03 15:53:06 +0700

    lol

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,8 +2,8 @@ module.exports.renderIndex = (req, res, next) => {
   res.render("index", { show: true, search: true });
 };
 module.exports.renderUpload = (req, res, next) => {
-  res.render("upload", { show: true, search: false });
+  res.render("upload", { show: true, search: false});
 };
 module.exports.renderManage = (req, res) => {
-  res.render("manage", { show: true, search: false });
+  res.render("manage", { show: true, search: false});
 };
