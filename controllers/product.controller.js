54d456d45d37f7e0b6a4ffae510da6e619f0d64b
vinhnt_ <thanhvinh2ker@gmail.com>
2020-04-03 10:40:35 +0700

    add

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,9 +1,9 @@
 module.exports.renderIndex = (req, res, next) => {
-  res.render("index",{show:true});
+  res.render("index", { show: true });
 };
 module.exports.renderUpload = (req, res, next) => {
-  res.render("upload",{show:true});
+  res.render("upload", { show: true });
 };
-module.exports.renderManage=(req,res)=>{
-  res.render("manage",{show:true})
-}
+module.exports.renderManage = (req, res) => {
+  res.render("manage", { show: true });
+};
