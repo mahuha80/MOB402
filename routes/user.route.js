1428b02c37da2ea7b00ee9dff3df5c78b0994a00
mahuha80 <dracula01112000@gmail.com>
2020-04-25 22:45:03 +0700

    done

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,11 @@
 var express = require("express");
 var router = express.Router();
 var controller = require("../controllers/user.controller");
+const middleware = require('../middlewares/checkLogin.middleware')
 router.get("/", controller.renderLogin);
 router.get("/register", controller.renderRegister);
 router.get("/login", controller.renderLogin);
 router.post("/register", controller.RegisterNewUser);
 router.post("/login", controller.LoginUser);
-router.get("/manage",controller.manageUser)
+router.get("/manage",middleware.requireAuth,controller.manageUser)
 module.exports = router;
